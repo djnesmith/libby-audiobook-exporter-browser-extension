1c6dde81f8f94df4b77bb8983f5a9a0235061d0d
HOU Tianze <houtianze@users.noreply.github.com>
2023-07-13 23:47:41 +0800

    Randomize wait time; Make download list scrollable when long

--- scripts/content.js
+++ scripts/content.js
@@ -9,9 +9,17 @@ const DOWNLOAD_LIST_ID = "lae-download-list"
 const PROGRESS_BAR_ID = "lae-progress-bar"
 const PROGRESS_ID = "lae-progress"
 
-const bannerText = "\u2190 to download | toggle download list \u2192";
+const INITIAL_BANNER = "Initializing download list...";
+const READY_BANNER = "\u2190 download | toggle list \u2192";
+const FAILED_BANNER = "Failed to retrieve the list, please refresh your tab (Ctrl + R)"
+
+const SkyBlue = "#87ceeb";
+const SpringGreen = "#00ff7f";
+const Crimson = "#dc143c";
 
 const delayMs = ms => new Promise(res => setTimeout(res, ms));
+const delayMsWithRatio = async (ms, ratio) => await delayMs(ms * (1 - ratio + 2 * ratio * Math.random()));
+const delayRoughlyMs = async (ms) => await delayMs(ms, 0.5)
 
 function createDownloadList() {
     const theDiv = document.createElement("div")
@@ -36,6 +44,7 @@ async function exportAudio() {
     const total = Object.keys(downloadMap).length;
     let current = 0;
     const progress = document.getElementById(PROGRESS_ID);
+    progress.style.backgroundColor = SpringGreen;
     for await (const url of Object.keys(downloadMap)) {
         const filename = downloadMap[url];
         console.log(`[lae] downloading ${url} as ${filename}`)
@@ -45,19 +54,17 @@ async function exportAudio() {
             filename: filename,
         })
         current++;
-        progress.style.backgroundColor = "lightgreen";
         progress.style.width = `${current * 100 / total}%`;
         progress.textContent = `${current} / ${total}`;
-        await delayMs(5000);
+        await delayRoughlyMs(5000);
     }
 
     // restore the progress bar
     progress.width = "100%";
-    progress.style.backgroundColor = "lightblue";
+    progress.style.backgroundColor = SkyBlue;
     progress.textContent = "All done!";
-    await delayMs(5000);
-    progress.style.backgroundColor = "";
-    progress.textContent = bannerText;
+    await delayRoughlyMs(5000);
+    setReadyBanner();
     console.log(`[lae] all files are downloaded.`)
 }
 
@@ -77,7 +84,7 @@ function attachElements() {
             class="nav-action-item"
         >Export</button>
         <div id="${PROGRESS_BAR_ID}">
-            <div id="${PROGRESS_ID}">${bannerText}</div>
+            <div id="${PROGRESS_ID}">${INITIAL_BANNER}</div>
         </div>
         <button id="${TOGGLE_LIST_BUTTON_ID}"
             class="nav-action-item"
@@ -95,18 +102,55 @@ function attachElements() {
         document.body.insertBefore(laeDiv, document.body.firstChild);
     }
 
-    const downloadList = createDownloadList();
-    // the download list above is regenerated, so we replace dom to update also
-    laeDiv.querySelector(`#${DOWNLOAD_LIST_ID}`).replaceWith(downloadList);
     laeDiv.querySelector(`#${EXPORT_BUTTON_ID}`).onclick = exportAudio
     laeDiv.querySelector(`#${TOGGLE_LIST_BUTTON_ID}`).onclick = toggleDownloadList
 }
 
+function attachDownloadList() {
+    const downloadList = createDownloadList();
+    const laeDiv = document.getElementById(LAE_CONTAINER_ID);
+    // the download list above is regenerated, so we replace dom to update also
+    laeDiv.querySelector(`#${DOWNLOAD_LIST_ID}`).replaceWith(downloadList);
+}
+
+function getProgressElement() {
+    return document.getElementById(PROGRESS_ID);
+}
+
+function setInitialBanner() {
+    const progress = getProgressElement();
+    progress.textContent = INITIAL_BANNER;
+    progress.style.backgroundColor = SkyBlue;
+}
+
+function setReadyBanner() {
+    const progress = getProgressElement();
+    progress.textContent = READY_BANNER;
+    progress.style.backgroundColor = "";
+}
+
+function setFailedBanner() {
+    const progress = getProgressElement();
+    progress.textContent = FAILED_BANNER;
+    progress.style.backgroundColor = Crimson
+}
+
+attachElements();
+setInitialBanner();
+attachDownloadList();
+
 (async () => {
+    const startMs = Date.now();
+    let timeOuted = false;
     while (Object.keys(downloadMap).length <= 2) { // cover and openbook.json
-        await delayMs(5000);
+        if ((Date.now() - startMs) > 30 * 1000) {
+            timeOuted = true;
+            break;
+        }
+        await delayRoughlyMs(5000);
         const response = await chrome.runtime.sendMessage({ command: "GetMap" });
         downloadMap = response?.downloadMap
     }
-    attachElements();
+    attachDownloadList();
+    timeOuted ? setFailedBanner() : setReadyBanner();
 })();
