171a2140d8d44812ff2084e23311b7d49d431ae1
HOU Tianze <houtianze@users.noreply.github.com>
2023-09-27 05:52:13 +0800

    Trivial: Optional chaining

--- scripts/content.js
+++ scripts/content.js
@@ -4,7 +4,7 @@ const LAE_UPDATE_NOTICE_ID = 'lae-update-notice'
 async function main() {
     chrome.storage.local.get(console.log)
     const notified = await chrome.storage.local.get('lae_update_notified')
-    if (Date.now() < ENDING_DATE.getTime() && !notified.lae_update_notified) {
+    if (Date.now() < ENDING_DATE.getTime() && !notified?.lae_update_notified) {
         const noticeHtml = `Libby Audio Extractor now uses popup (just click the extension icon) instead of inserting elements to the page. (Click to permanently dismiss this notice)`
         const noticeDiv = document.createElement("div");
         noticeDiv.id = LAE_UPDATE_NOTICE_ID
